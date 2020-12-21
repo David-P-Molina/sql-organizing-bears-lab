f5c65113fa0954333a0adeb94edb546df4bf900d
David Moina <davemolina777@gmail.com>
2020-12-21 17:20:50 -0600

    commit tree 11884596d0d36a596d9eab355ac4de85011b0130

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears  VALUES (1, "Mr. Chocolate", 4, "M", "Brown", "Hyper", true);
+INSERT INTO bears  VALUES (2, "Rowdy", 6, "M", "White", "Kind", false);
+INSERT INTO bears  VALUES (3, "Tabitha", 8, "F", "Black", "Jittery", false);
+INSERT INTO bears  VALUES (4, "Sergeant Brown", 2, "F", "Black", "Happy", true);
+INSERT INTO bears  VALUES (5, "Melissa", 1, "F", "Brown", "Energetic", true);
+INSERT INTO bears  VALUES (6, "Grinch", 9, "M", "Black", "Angry", false);
+INSERT INTO bears  VALUES (7, "Wendy", 11, "F", "Brown", "Goofy", false);
+INSERT INTO bears  VALUES (8, NULL, 5, "M", "White", "Violent", true);
